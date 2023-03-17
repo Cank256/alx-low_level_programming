114f922cb9b51ac63cb47fe6cada21c948e01c76
Nkunze Caleb <nkunzecaleb@gmail.com>
2023-03-17 07:17:19 -0700

    write

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+/**
+ * main - Prints a message to the standard error
+ * Return: Always 1 (error code)
+ */
+#include <unistd.h>
+int main(void)
+{
+char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+int length = 59;
+int i;
+for (i = 0; i < length; i++)
+{
+write(2, &message[i], 1);
+}
+return (1);
+}
