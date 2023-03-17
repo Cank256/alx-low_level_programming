d5833e9eb925bc386767cbdf64d440b3c9a3472f
Nkunze Caleb <nkunzecaleb@gmail.com>
2023-03-17 07:20:05 -0700

    write update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,7 @@
 #include <unistd.h>
 int main(void)
 {
-char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+char *message="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 int length = 59;
 int i;
 for (i = 0; i < length; i++)
