d52c3d3b227ec2442638ae0ef37d7c3cf36c7a05
Nkunze Caleb <nkunzecaleb@gmail.com>
2023-03-17 07:47:43 -0700

    positive and anegative update

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,19 +1,17 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
-
 /**
  * main - Prints whether a randomly generated number is positive or negative
  *
  * Return: Always 0.
  */
-
 int main(void)
 {
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-printf("Number: %d\n", n);        
+printf("%d", n);        
 if (n > 0){
 printf("is positive\n");
 }else if (n == 0){
