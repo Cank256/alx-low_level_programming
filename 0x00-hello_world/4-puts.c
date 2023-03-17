0f0e583096073cb64fbe28dd4d50b4f947b8e2a9
Nkunze Caleb <nkunzecaleb@gmail.com>
2023-03-17 06:36:36 -0700

    add comment

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,6 @@
 /* Program that uses puts to print string */
 #include <stdio.h>
+/* program that uses puts function */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
