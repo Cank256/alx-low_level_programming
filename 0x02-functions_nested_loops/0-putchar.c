4c892236514165dc6d1d88f4a2ed8732fe6f4867
Nkunze Caleb <nkunzecaleb@gmail.com>
2023-03-22 23:27:49 -0700

    Correct Print

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,6 +7,6 @@
  */
 int main()
 {
-print("_putchar\n");
+printf("_putchar\n");
 return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,8 @@
 #ifndef MAIN_H_
 #define MAIN_H_
 
-// Function prototypes
 void foo();
 int bar(int x, int y);
 float baz(float a, float b, float c);
 
-#endif // MAIN_H_
+#endif
