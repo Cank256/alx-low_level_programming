7c9991ff12984cf46bbaeb02b3de36c5450563b8
Nkunze Caleb <nkunzecaleb@gmail.com>
2023-03-23 00:06:22 -0700

    update prototype

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,7 +5,4 @@
  *
  * Return: 1 if c is a letter, lowercase or uppercase, 0 otherwise
  */
-int _isalpha(int c)
-{
-return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
-}
+int _isalpha(int c);
