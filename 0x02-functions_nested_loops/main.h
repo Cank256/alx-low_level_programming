91f8b45b40721c919836c7da879807b12922b3a4
Nkunze Caleb <nkunzecaleb@gmail.com>
2023-03-22 23:42:16 -0700

    Rectify main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,21 @@
 #ifndef MAIN_H_
 #define MAIN_H_
+#include <unistd.h>
 
 void foo();
 int bar(int x, int y);
 float baz(float a, float b, float c);
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
 
 #endif
