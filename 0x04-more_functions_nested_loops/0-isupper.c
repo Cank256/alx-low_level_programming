a93c695c0002d45e4013c0fac809caf14a68cb1e
Nkunze Caleb <nkunzecaleb@gmail.com>
2023-03-24 02:08:51 -0700

    is upper case

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * function to check if the character is in uppercase
+ *
+ * Return: (1) Success
+ */
+void _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
