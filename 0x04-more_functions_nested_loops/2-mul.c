f388c77d36d543837434f67a8ce8daf28ec62d8c
Nkunze Caleb <nkunzecaleb@gmail.com>
2023-03-24 02:42:45 -0700

    betty in the face

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -4,12 +4,12 @@
 /**
  * mul - multiply two integers
  *
- * @a - first number
- * @b - second number
+ * @a: first number
+ * @b: second number
  *
  * Return: the product of the two integers
  */
 int mul(int a, int b)
 {
-return (a*b);
+return (a * b);
 }
