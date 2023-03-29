5e818f467c688b2823e13587de386cb339197480
Nkunze Caleb <nkunzecaleb@gmail.com>
2023-03-29 11:17:32 -0700

    one and two are a go

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,22 @@
+/**
+ * _strncat - Concatenates two strings.
+ * @dest: Pointer to the destination string.
+ * @src: Pointer to the source string.
+ * @n: Maximum number of bytes to copy from src.
+ *
+ * Return: Pointer to the resulting string dest.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int dest_len = strlen(dest);
+int i;
+
+for (i = 0; i < n && src[i] != '\0'; i++)
+{
+dest[dest_len + i] = src[i];
+}
+
+dest[dest_len + i] = '\0';
+
+return dest;
+}
