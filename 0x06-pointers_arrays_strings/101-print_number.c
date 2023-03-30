6ede38ac349a7be431599ff51520c548b0308051
Nkunze Caleb <nkunzecaleb@gmail.com>
2023-03-30 01:57:04 -0700

    print number

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * print_number - prints an integer
+ * @n: the integer to print
+ */
+
+void print_number(int n)
+{
+int digit, is_negative = 0;
+unsigned int num;
+
+if (n < 0)
+{
+is_negative = 1;
+num = -n;
+}
+else
+{
+num = n;
+}
+
+if (num / 10 != 0)
+{
+print_number(num / 10);
+}
+
+digit = num % 10;
+
+if (is_negative)
+{
+_putchar('-');
+}
+
+_putchar(digit + '0');
+}
+
