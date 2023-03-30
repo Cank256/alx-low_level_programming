fa42dc537ccae6018ffec3e9e28cc17f96ddcd78
Nkunze Caleb <nkunzecaleb@gmail.com>
2023-03-30 02:10:32 -0700

    more magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 
+/**
+ * Function that makes magic
+ *
+ * Return: (0) Always
+ */
 int main(void)
 {
-  int n;
-  int a[5];
-  int *p;
+int n;
+int a[5];
+int *p;
 
-  a[2] = 1024;
-  p = &n;
-  *(a + 1) = 98;
+a[2] = 1024;
+p = &n;
+*(a + 1) = 98;
+printf("a[2] = %d\n", a[2]);
   
-  printf("a[2] = %d\n", a[2]);
-  return (0);
+return (0);
 }
