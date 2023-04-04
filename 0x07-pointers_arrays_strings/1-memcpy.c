9f3bf50840bf8efb0459bb12bb0805f5be05b66a
Nkunze Caleb <nkunzecaleb@gmail.com>
2023-04-03 23:22:29 -0700

    copy memory bytes

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _memcpy - occupies memory
+ * @dest: Pointer to memory area to be filled
+ * @src: Pointer of memory to copy from
+ * @n: Number of bytes of source memory area
+ *
+ * Result: Pointer to destinaion memory
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+
+unsigned int i;
+
+for (i = 0; i < n; i++)
+{
+dest[i] = src[i];
+}
+
+return (dest);
+}
