33d31532a4c6fe903d6e1e14a343fc0463c6d47a
Nkunze Caleb <nkunzecaleb@gmail.com>
2023-04-05 11:50:31 -0700

    square root

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdio.h>
+
+int _sqrt_helper(int n, int min, int max);
+
+/**
+ * _sqrt_recursion - Function that returns the
+ *                   natural square root of a number
+ * @n: Number to get square root
+ *
+ * Return: Square root of a number
+ */
+int _sqrt_recursion(int n)
+{
+
+if (n < 0)
+{
+return (-1);
+}
+else
+{
+return (_sqrt_helper(n, 0, n));
+}
+}
+
+int _sqrt_helper(int n, int min, int max)
+{
+if (max < min)
+{
+return -1;
+}
+
+int guess = (min + max) / 2;
+
+if (guess * guess == n)
+{
+return guess;
+}
+else if (guess * guess < n)
+{
+return _sqrt_helper(n, guess + 1, max);
+}
+else
+{
+return _sqrt_helper(n, min, guess - 1);
+}
+}
