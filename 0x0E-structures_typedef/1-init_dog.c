2ee02c2631d2cc49c55ac4f76c0607574cd456cb
Nkunze Caleb <nkunzecaleb@gmail.com>
2023-04-17 23:13:13 -0700

    function to initialize dog update

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,3 +1,4 @@
+#include "dog.h"
 /**
  * init_dog - unction that initialize a variable of type struct dog
  *
@@ -8,8 +9,6 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-d = &dog;
-
 d->name = name;
 d->age = age;
 d->owner = owner;
