7665abf930e97cadc3210aa3b69d7a9e898917f9
Nkunze Caleb <nkunzecaleb@gmail.com>
2023-04-27 11:07:09 -0700

    start the fun

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * startupfun - Prints "You're beat! and yet, you must allow,
+ *              \nI bore my house upon my back!\n"
+ *               before the main function is executed.
+ **/
+void startupfun(void) __attribute__ ((constructor));
+void startupfun(void)
+{
+printf("You're beat! and yet, you must allow,
+\nI bore my house upon my back!\n");
+}
+
