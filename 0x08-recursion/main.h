18001693a3589edb737f772507e5cd320d14edfb
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-06-07 09:11:29 +0300

    add recursion checkers for sqrt and palindrome

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -7,6 +7,8 @@ void _print_rev_recursion(char *s);
 
 int _strlen_recursion(char *s);
 
+int _sqrt_helper(int n, int guess);
+
 int factorial(int n);
 
 int _pow_recursion(int x, int y);
@@ -16,6 +18,8 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 
 int is_palindrome(char *s);
+
+int check_palindrome(char *s, int start, int end);
 
 int wildcmp(char *s1, char *s2);
 
