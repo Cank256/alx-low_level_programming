1cfde6df8bf7a8242afa71b2ab95812b6d4698f2
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-06-12 18:53:22 +0300

    update string concatination

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,9 +12,9 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-size_t s1_len = strlen(s1);
-size_t s2_len = strlen(s2);
-char *result = (char *)malloc(s1_len + s2_len + 1);
+size_t s1_len;
+size_t s2_len;
+char *result;
 
 if (s1 == NULL)
 {
@@ -24,6 +24,10 @@ if (s2 == NULL)
 {
 s2 = "";
 }
+
+s1_len = strlen(s1);
+s2_len = strlen(s2);
+result = (char *)malloc(s1_len + s2_len + 1);
 if (result == NULL)
 {
 return (NULL);
