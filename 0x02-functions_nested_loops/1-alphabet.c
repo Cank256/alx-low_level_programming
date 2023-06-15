24d85958ca01fd53f4cb9c9a0af1cea38723dc73
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-06-15 15:59:27 +0300

    update alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,8 +3,6 @@
 
 /**
  * print_alphabet - Prints the alphabet in lowercase
- *
- * Return: Always 0 (Success)
  */
 void print_alphabet(void)
 {
@@ -15,16 +13,4 @@ _putchar(letter);
 letter++;
 }
 _putchar('\n');
-return (0);
-}
-
-/**
- * main - Entry point of the program
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-print_alphabet();
-return (0);
 }
