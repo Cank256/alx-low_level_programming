e23e1d791fa906912415e92ca34fd29e77fb1935
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-06-15 22:06:47 +0300

    update natural numbers

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -9,16 +9,19 @@
  *
  * Return: Always 0 (Success)
  */
-int main()
+int main(void)
 {
 int sum = 0;
-for (int i = 0; i < 1024; i++)
+
+for (int i = 1; i < 1024; i++)
 {
 if (i % 3 == 0 || i % 5 == 0)
 {
 sum += i;
 }
 }
+
 printf("%d\n", sum);
-return 0;
+
+return (0);
 }
