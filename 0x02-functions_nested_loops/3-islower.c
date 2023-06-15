d03c884bb32b1ccbb4b1547ad3926a861d8a4568
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-06-15 20:48:58 +0300

    remove main function inside the islower file

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -18,31 +18,3 @@ else
 return (0);
 }
 }
-
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-char c1 = 'a';
-char c2 = 'A';
-if (_islower(c1))
-{
-printf("%c is lowercase\n", c1);
-}
-else
-{
-printf("%c is not lowercase\n", c1);
-}
-if (_islower(c2))
-{
-printf("%c is lowercase\n", c2);
-}
-else
-{
-printf("%c is not lowercase\n", c2);
-}
-return (0);
-}
