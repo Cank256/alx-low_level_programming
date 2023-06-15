f2e56de31515ed3e39a5e513729445fa789d0b73
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-06-15 21:07:41 +0300

    update abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,14 +1,19 @@
-/*
- * Return: (n)
+#include "main.h"
+
+/**
+ * print_sign - Prints the sign of a number
+ * @n: The number to check
+ *
+ * Return: 1 if n is greater than zero, 0 if n is zero, -1 if n is less than zero
  */
 int _abs(int n)
 {
 if (n < 0)
 {
-return -n;
+return (-n);
 }
 else
 {
-return n;
+return (n);
 }
 }
