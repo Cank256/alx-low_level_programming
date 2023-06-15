fcd47ee8f9063d7c9186300da421f01d8d145ca0
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-06-15 08:27:08 +0300

    Add _putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,16 +4,6 @@
 #include <ctype.h>
 #include <assert.h>
 
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-return (write(1, &c, 1));
-}
+int _putchar(char c);
 
 #endif
