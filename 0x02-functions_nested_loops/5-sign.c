3e66f2fe664bfc60f8ea6f2fa110aadec2c57c05
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-06-15 20:54:55 +0300

    update print_sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,14 +1,22 @@
+#include "main.h"
+
+/**
+ * print_sign - Prints the sign of a number
+ * @n: The number to check
+ *
+ * Return: 1 if n is positive, 0 if n is zero, -1 if n is negative
+ */
 int print_sign(int n)
 {
 if (n > 0)
 {
 printf("+");
-return 1;
+return (1);
 }
 else if (n == 0)
 {
 printf("0");
-return 0;
+return (0);
 }
 else
 {
