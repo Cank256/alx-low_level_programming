ccd14ce166c970cf272b302e32098a75e669a52b
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-06-15 21:24:48 +0300

    update times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,6 +1,13 @@
+#include "main.h"
+
+/**
+ * times_table - Prints the multiplication table for the number 9
+ */
 void times_table(void)
 {
-for (int i = 0; i <= 10; i++)
+int i;
+
+for (i = 0; i <= 10; i++)
 {
 printf("9 x %d = %d\n", i, 9*i);
 }
