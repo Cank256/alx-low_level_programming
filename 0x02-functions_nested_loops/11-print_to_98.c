314756a64cdf464f2ed465d8a667bb134bc51477
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-06-15 21:56:48 +0300

    update print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,9 +9,11 @@
  */
 void print_to_98(int n)
 {
+int i;
+
 if (n > 98)
 {
-for (int i = n; i >= 98; i--)
+for (i = n; i >= 98; i--)
 {
 printf("%d", i);
 if (i != 98)
@@ -22,7 +24,7 @@ printf(", ");
 }
 else
 {
-for (int i = n; i <= 98; i++)
+for (i = n; i <= 98; i++)
 {
 printf("%d", i);
 if (i != 98)
