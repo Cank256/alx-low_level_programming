8236f54e15650d78e9b89aa165c35e71eab9f53e
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-06-15 15:57:09 +0300

    issues with betty

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,15 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 
-/*
- * Using prototype
- * Return: (0)
+/**
+ * print_alphabet - Prints the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
  */
-
 void print_alphabet(void)
 {
 char letter = 'a';
-while(letter<='z')
+while (letter <= 'z')
 {
 _putchar(letter);
 letter++;
@@ -18,8 +18,11 @@ _putchar('\n');
 return (0);
 }
 
-void print_alphabet(void);
-
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 print_alphabet();
