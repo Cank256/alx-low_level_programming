4a98ebd467f15578c11bf2d325a06a906a9c18d6
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-06-15 21:59:24 +0300

    times_table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,16 +1,27 @@
+#include "main.h"
+
+/**
+ * print_times_table - Prints the times table of a given number
+ * @n: The number to generate the times table for
+ *
+ * Description: This function prints the times table of the given number,
+ * ranging from 0 to n. The table is printed in a tabular format.
+ * The function handles cases where n is negative or greater than 15.
+ */
 void print_times_table(int n)
 {
-// Check if n is within the valid range
+int i, j;
+
 if (n < 0 || n > 15)
 {
 return;
 }
-// Print the times table
-for (int i = 0; i <= n; i++)
+
+for (i = 0; i <= n; i++)
 {
-for (int j = 0; j <= n; j++)
+for (j = 0; j <= n; j++)
 {
-printf("%d\t", i*j);
+printf("%d\t", i * j);
 }
 printf("\n");
 }
