af1d312b58824d7f59356a0fbf1c2d481526a851
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-06-19 09:48:21 +0300

    update _strlen error

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,6 +1,28 @@
 #include "main.h"
 
 /**
+ * _strlen - Calculates the length of a string.
+ * @s: Pointer to the string.
+ *
+ * Return: The length of the string.
+ *
+ * Description: This function takes a string pointer as a parameter
+ * and returns the length of the string.
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+
+	return (length);
+}
+
+/**
  * rev_string - Reverses a string.
  * @s: Pointer to the string to be reversed.
  *
