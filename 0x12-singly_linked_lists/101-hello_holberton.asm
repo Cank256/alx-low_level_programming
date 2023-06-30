e42ed6692891377b010ce651014bf8d35cdb3d3e
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-06-30 18:43:24 +0300

    update ASM

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,17 +1,18 @@
 section .data
-    hello: db 'Hello, Holberton', 10, 0   ; define the string with a newline character and null terminator
-    fmt: db '%s', 10, 0                   ; define the format string with a newline character and null terminator
+    hello db "Hello, Holberton", 0
 
 section .text
-    global _start
+    extern puts
+
+global _start
 
 _start:
-    mov rax, 0          ; set rax to 0 for printf call
-    lea rdi, [hello]    ; load the address of the hello string into rdi
-    mov rsi, fmt        ; load the address of the format string into rsi
-    mov rdx, 0          ; set rdx to 0 for printf call
-    call printf         ; call printf function
-    mov rax, 60         ; set rax to 60 for exit syscall
-    xor rdi, rdi        ; set rdi to 0 for exit status
-    syscall             ; call exit syscall
+    ; Push the address of the hello string as an argument to puts
+    mov rdi, hello
+    ; Call puts
+    call puts
 
+    ; Exit the program
+    mov eax, 60 ; System call number for exit
+    xor edi, edi ; Exit status 0
+    syscall
