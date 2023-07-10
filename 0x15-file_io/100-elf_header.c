f6bbd2baebda54f7701306c64b385326ccfc731e
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-07-10 13:24:04 +0300

    update

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -20,7 +20,8 @@ void print_elf_header(Elf64_Ehdr *header)
 {
 printf("ELF Header:\n");
 printf("  Magic:   ");
-for (int i = 0; i < EI_NIDENT; i++)
+int i;
+for (i = 0; i < EI_NIDENT; i++)
 {
 printf("%02x ", header->e_ident[i]);
 }
