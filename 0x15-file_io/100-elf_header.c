8768adc4f8627a6afac3c378fa137fcd4d340549
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-07-10 13:25:21 +0300

    update elf header function

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -18,9 +18,9 @@ exit(code);
  */
 void print_elf_header(Elf64_Ehdr *header)
 {
+int i;
 printf("ELF Header:\n");
 printf("  Magic:   ");
-int i;
 for (i = 0; i < EI_NIDENT; i++)
 {
 printf("%02x ", header->e_ident[i]);
