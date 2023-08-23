c32137117152567853c11bf5180a51b52454abe2
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-08-23 09:32:45 +0300

    update

---
+++ 0x18-dynamic_libraries/_atoi.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _atoi - Converts a string to an integer
+ * @s: The string to convert
+ *
+ * Return: The converted integer.
+ */
+int _atoi(char *s)
+{
+return (0);
+}
