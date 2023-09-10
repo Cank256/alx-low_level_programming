5f291659f03fa496802c7d257ecfcfc01e49c4fe
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-09-10 11:06:00 +0300

    updated hash table create

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -15,23 +15,23 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
+hash_table_t *new_table;
 unsigned long int i;
-hash_table_t *new_table = (hash_table_t *)malloc(sizeof(hash_table_t));
 
-if (size <= 0)
+if (size == 0)
 {
 return (NULL);
 }
 
-if (new_table == NULL)
+new_table = malloc(sizeof(hash_table_t));
+if (!new_table)
 {
 return (NULL);
 }
 
 new_table->size = size;
-new_table->array = (HashNode **)malloc(sizeof(HashNode *) * size);
-
-if (new_table->array == NULL)
+new_table->array = malloc(sizeof(hash_node_t *) * size);
+if (!new_table->array)
 {
 free(new_table);
 return (NULL);
