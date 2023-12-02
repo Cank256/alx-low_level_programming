493410c928d97d64137ac604b34b2e2b2735b4b4
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-12-02 10:57:09 +0300

    nested loop update

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,11 +8,16 @@
  */
 int print_last_digit(int n)
 {
-int last_digit = n % 10;
-if (last_digit < 0)
+int last_digit;
+
+if (n < 0)
 {
-last_digit = -last_digit;
+n = -n;
 }
-printf("%d", last_digit);
+
+last_digit = n % 10;
+
+_putchar(last_digit + '0');
+
 return (last_digit);
 }
